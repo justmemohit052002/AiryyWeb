62319eb81b7a14061e149fb8c082b9b390ead3d9
Priyanshu joshi <prjoshi2710@gmail.com>
2023-12-28 20:33:03 +0530

    service section added but write it is not responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,15 +16,14 @@ const Hero = () => {
 
       <div className="absolute sm:hidden   left-[20px] sm:left-[55px]">
         <div className="relative z-10 top-[210px] ">
-
-          <motion.span className="text-yellow-500 xs:text-5xl ss:text-5xl  sm:text-6xl   font-medium font-['Fredoka'] uppercase leading-[48px] tracking-[2.88px]">
+          <motion.span className="text-yellow-500 xs:text-5xl ss:text-5xl  sm:text-6xl   font-medium font-poppins uppercase leading-[48px] tracking-[2.88px]">
             Great Rides
           </motion.span>
 
-          <motion.span className="text-yellow-400 text-opacity-80 xs:text-5xl ss:text-5xl  sm:text-6xl   font-medium font-['Fredoka'] uppercase leading-[48px] tracking-[2.88px]">
+          <motion.span className="text-yellow-400 text-opacity-80 xs:text-5xl ss:text-5xl  sm:text-6xl   font-medium font-poppins uppercase leading-[48px] tracking-[2.88px]">
             !<br />
           </motion.span>
-          <motion.span className="text-white xs:text-2xl ss:text-3xl sm:text-4xl  font-medium font-['Fredoka'] uppercase leading-[48px] tracking-widest">
+          <motion.span className="text-white xs:text-2xl ss:text-3xl sm:text-4xl  font-medium font-poppins uppercase leading-[48px] tracking-widest">
             Rent-Return-Repete
           </motion.span>
           <motion.span className="text-white text-[45px] font-medium font-['Fredoka'] capitalize leading-[48px] tracking-[5.40px]">
@@ -34,7 +33,7 @@ const Hero = () => {
 
         <div className=" max-w-1 cursor-pointer sm:hidden relative top-[240px] z-full  z-10">
           <div className="w-[145px] h-[38px] px-4 py-5 bg-white rounded-md justify-center items-center gap-3.5 inline-flex">
-            <div className="text-black text-sm font-semibold font-['Inter'] leading-[18px]">
+            <div className="text-black text-sm font-semibold font-poppins leading-[18px]">
               Contact Now
             </div>
           </div>
@@ -43,20 +42,23 @@ const Hero = () => {
 
       {/* Screen */}
       <div className=" xs:hidden ss:hidden h-[135px] left-[46px]  lg:left-[90px]  top-[344px] lg:top-[254px] z-20 absolute">
-        <div className="w-[483.30px] text-yellow-500 left-0 top-0 absolute text-[54px] font-medium font-['Fredoka'] uppercase tracking-[6.48px]">
+        <div className="w-[483.30px] text-yellow-500 left-0 top-0 absolute text-[54px] font-medium font-poppins uppercase tracking-[6.48px]">
           AiRYY Rides!
         </div>
-        <div className="w-[565px] left-0 top-[112px] absolute text-[37px] font-medium font-['Fredoka'] uppercase leading-[23px] tracking-[4.44px]">
+        <div className="w-[565px] left-0 top-[112px] absolute text-[37px] font-medium font-poppins uppercase leading-[23px] tracking-[4.44px]">
           Rent, Return, Repeat.
         </div>
       </div>
 
-      <div className="xs:hidden ss:hidden w-[595px] h-[49px] left-[46px]  md:left-[45px]  lg:left-[90px] top-[539px] lg:top-[439px] z-20 absolute text-white text-[19px] font-semibold font-['Inter'] leading-[18px]">
+      <div className="xs:hidden ss:hidden w-[595px] h-[49px] left-[46px]  md:left-[45px]  lg:left-[90px] top-[539px] lg:top-[439px] z-20 absolute text-white text-[19px] font-semibold font-poppins leading-[18px]">
         Your journey, your vehicle – experience travel like never before.
         <br />
       </div>
       <div className="xs:hidden cursor-pointer ss:hidden px-4 py-2.5  left-[46px]  md:left-[45px]  lg:left-[90px] top-[627px] lg:top-[527px] absolute z-20 rounded-[10px] border-2 border-yellow-500 justify-start items-start gap-2 inline-flex">
-        <a  onClick={() => scrollToSection("About")} className="text-yellow-400 text-[15px] font-semibold font-['Inter'] leading-[18px]">
+        <a
+          onClick={() => scrollToSection("About")}
+          className="text-yellow-400 text-[15px] font-semibold font-['Inter'] leading-[18px]"
+        >
           Learn More
         </a>
       </div>
@@ -70,7 +72,7 @@ const Hero = () => {
           style={{ borderRadius: 9999, position: "absolute" }}
         /> */}
         <ImageCarousel />
-       
+
         <div className=" absolute xs:-left-[45vw] xs:-top-[20vh] ss:-left-[45vw] ss:-top-[18vh]  sm:left-[10vw] sm:-top-[15vh] xl:-top-[25ch]  -z-10">
           <div
             className="absolute h-[1000px] xl:h-[1300px] w-[1450px]  sm:h-[130rem] sm:w-[205rem] bg-gradient-to-b from-yellow-400 via-transparent to-transparent rounded-[743.5px/595.5px]"
@@ -79,40 +81,37 @@ const Hero = () => {
             }}
           />
           <div className=" absolute  z-100 top-0 bottom-0 -left-20">
-
-           <svg
-           
-           className="absolute  h-[900px] w-[1450px] " 
-          width="316"
-          height="269"
-          viewBox="0 0 316 269"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <ellipse
-            cx="228.5"
-            cy="183"
-            rx="228.5"
-            ry="183"
-            fill="url(#paint0_linear_85_169)"
-          />
-          <defs>
-            <linearGradient
-              id="paint0_linear_85_169"
-              x1="91.2817"
-              y1="303.56"
-              x2="387.491"
-              y2="188.619"
-              gradientUnits="userSpaceOnUse"
+            <svg
+              className="absolute  h-[900px] w-[1450px] "
+              width="316"
+              height="269"
+              viewBox="0 0 316 269"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
             >
-              <stop stop-color="#11100F" stop-opacity="0.3" />
-              <stop offset="1" stop-color="#EEC800" stop-opacity="0" />
-            </linearGradient>
-          </defs>
-        </svg>
+              <ellipse
+                cx="228.5"
+                cy="183"
+                rx="228.5"
+                ry="183"
+                fill="url(#paint0_linear_85_169)"
+              />
+              <defs>
+                <linearGradient
+                  id="paint0_linear_85_169"
+                  x1="91.2817"
+                  y1="303.56"
+                  x2="387.491"
+                  y2="188.619"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop stop-color="#11100F" stop-opacity="0.3" />
+                  <stop offset="1" stop-color="#EEC800" stop-opacity="0" />
+                </linearGradient>
+              </defs>
+            </svg>
           </div>
         </div>
-       
       </div>
 
       {/* Screen Image */}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,24 +15,23 @@ const Navbar = () => {
     <>
       <div className=" fixed z-[100]">
         <div className="xs:hidden bg-blend-color bg-no-repeat ss:hidden  sm:hidden absolute md:flex   md:left-[10px]    lg:left-[60px] xl:left-[20px] top-[20px] lg:top-[0px]  w-screen   z-[100] text-white md:p-8 lg:py-8 lg:px-12  flex-row items-center justify-between">
-          <img className="w-24 bg-blend-color bg-no-repeat" src={Logo} alt=''/>
-          
+          <img className="w-24 bg-blend-color bg-no-repeat" src={Logo} alt="" />
 
-          <div className="flex flex-row gap-4 lg:gap-8 relative md:-left-[100px] sm:text-[1.7rem] md:text-3xl lg:text-xl  items-center">
+          <div className="flex  flex-row gap-4 lg:gap-8 relative md:-left-[100px] sm:text-[1.7rem] md:text-3xl lg:text-xl  items-center">
             <a
-              className="cursor-pointer"
+              className="cursor-pointer font-poppins"
               onClick={() => scrollToSection("Hero")}
             >
               Home
             </a>
             <a
-              className="cursor-pointer"
+              className="cursor-pointer font-poppins"
               onClick={() => scrollToSection("About")}
             >
               About
             </a>
             <a
-              className="cursor-pointer"
+              className="cursor-pointer font-poppins"
               onClick={() => scrollToSection("Contact")}
             >
               Contact
@@ -40,60 +39,50 @@ const Navbar = () => {
           </div>
           <div className="w-[292.61px]   cursor-pointer  relative justify-start items-center gap-[25px] inline-flex">
             <div className="px-4 py-2.5 bg-white rounded-md justify-center items-center gap-2.5 flex">
-              <div className="text-black text-sm font-semibold font-['Inter'] leading-[18px]">
+              <div className="text-black text-sm font-semibold font-poppins leading-[18px]">
                 Contact Now
               </div>
             </div>
           </div>
         </div>
 
-
-
-
-
-
-        
-
         <div className="md:hidden sm:flex xs:text-4xl sm:text-5xl w-screen fixed z-[100] bg-[#000]  text-white p-8 xs:flex xs:flex-row ss:flex ss:flex-row items-center justify-between">
           {/* logo */}
           <div>AiRYY Rides</div>
           {/* menu */}
           <div className="">
-            <CiMenuBurger/>
+            <CiMenuBurger />
           </div>
         </div>
       </div>
 
-      
-      
-      
       <div className="xs:hidden ss:hidden sm:hidden  xl:hidden">
-      <div className="w-[1487px] h-[1191px] left-[281px] top-[153px] absolute bg-gradient-to-b from-yellow-400 via-neutral-950 to-transparent rounded-full" />
+        <div className="w-[1487px] h-[1191px] left-[281px] top-[153px] absolute bg-gradient-to-b from-yellow-400 via-neutral-950 to-transparent rounded-full" />
 
-      <div className="w-[872px] h-9 left-[76px] top-[55px] absolute justify-between items-center inline-flex">
-        <div className="w-24 h-[44px] justify-center items-center flex">
-        AiRYY Rides
-        </div>
-        
-        <div className="justify-start items-center gap-[45px] flex">
-          <div className="text-white text-base font-medium font-['Inter'] leading-none">
-            Home
+        <div className="w-[872px] h-9 left-[76px] top-[55px] absolute justify-between items-center inline-flex">
+          <div className="w-24 h-[44px] justify-center items-center flex">
+            AiRYY Rides
           </div>
-          <div className="text-white text-base font-medium font-['Inter'] leading-none">
-            About
+
+          <div className="justify-start items-center gap-[45px] flex">
+            <div className="text-white text-base font-medium font-['Inter'] leading-none">
+              Home
+            </div>
+            <div className="text-white text-base font-medium font-['Inter'] leading-none">
+              About
+            </div>
+            <div className="text-white text-base font-medium font-['Inter'] leading-none">
+              Contact
+            </div>
           </div>
-          <div className="text-white text-base font-medium font-['Inter'] leading-none">
-            Contact
-          </div>
-        </div>
-        <div className="justify-start items-center gap-[25px] flex">
-          <div className="px-4 py-2.5 bg-white rounded-md justify-center items-center gap-2.5 flex">
-            <div className="text-black text-sm font-semibold font-['Inter'] leading-[18px]">
-              Contact Now
+          <div className="justify-start items-center gap-[25px] flex">
+            <div className="px-4 py-2.5 bg-white rounded-md justify-center items-center gap-2.5 flex">
+              <div className="text-black text-sm font-semibold font-['Inter'] leading-[18px]">
+                Contact Now
+              </div>
             </div>
           </div>
         </div>
-      </div>
       </div>
     </>
   );
