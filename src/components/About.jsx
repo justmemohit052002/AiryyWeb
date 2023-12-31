f49dfd9fe865331c16bf235c51bc47dfcb3b2609
Mohit Singh <100300162+justmemohit052002@users.noreply.github.com>
2023-12-31 12:38:34 +0530

    Built About screen and Contact screen
    Co-Authored-By: Prashant Khanchandani <92585387+Piyush-K108@users.noreply.github.com>
    Co-Authored-By: Priyanshu Joshi <99650183+Priyanshu-27@users.noreply.github.com>

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,9 +2,45 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div id = "About" className='About h-screen'>
-      About
+    <div id="NewRootRoot" className="flex flex-row w-full items-start">
+      <div
+        id="AboutUs"
+        className="overflow-hidden bg-black relative flex flex-row w-full font-['Inter'] items-start pt-[213px] px-[209px]"
+      >
+        <div
+          id="Ellipse"
+          className="w-4/6 h-[970px] bg-[linear-gradient(180deg,_#ffd600_0%,rgba(10,_10,_9,_0)_88%)] bg-cover bg-50%_50% bg-blend-normal bg-no-repeat absolute top-[-45px] left-[-54px] flex flex-col gap-24 items-start pl-64 py-64 rounded-[50%]"
+        >
+          <div className="text-5xl font-semibold leading-[16px] text-white w-1/1">
+            About Us
+          </div>
+          <div className="text-xl font-semibold leading-[16px] text-white w-1/1">
+            AiRYY Rides! Ride, Return, Repeat.
+          </div>
+        </div>
+        <div
+          id="Ellipse1"
+          className="w-4/2 h-[1017px] bg-[linear-gradient(73deg,_rgba(17,_16,_15,_0.3)_13%,rgba(238,_200,_0,_0)_82%)] bg-cover bg-50%_50% bg-blend-normal bg-no-repeat absolute top-[-92px] left-[826px] flex flex-row justify-end items-start pt-[203px] px-16 rounded-[50%]"
+        >
+          <img
+            src="https://file.rendit.io/n/rH5LwF78dqiwZ10YKfVp.png"
+            alt="Vecteezypeopleworkingandmeetingillustration"
+          />
+        </div>
+        <div className="text-justify text-xl leading-[23px] text-white relative w-2/5 mt-40 mb-[213px]">
+          Welcome to AiRYY Rides, where we go beyond just providing bikes; we're
+          dedicated to creating unforgettable experiences.Our commitment to
+          sustainable urban mobility fuels our mission tooffer a diverse fleet of
+          meticulously maintained bikes, ensuring that every AiRYY ride is not only
+          smooth and safe but also packed with adventure. Visit us at Bhawarkua,
+          Indore, and embark on a journey with AiRYY that transforms your commute
+          into a memorable adventure. Get ready to explore the city with ease,
+          style, and a touch of excitement.
+          <br />
+        </div>
+      </div>
     </div>
+
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,41 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
-  content: ["./index.html", './src/**/*.{js,jsx}',
-  ],
+  darkMode: "class",
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
     container: {
       padding: {
-        DEFAULT: '15px',
+        DEFAULT: "15px",
       },
     },
     screens: {
-      xs:{'min':'320px','max':'440px'},
-      ss:{'min':'440px','max':'640px'},
-      sm: '640px',
-      md: '768px',
-      lg: '960px',
-      xl: '1024px',
-      xxl: '1200px',
-      img:{'min':'1200px','max':'1376px'}
+      xs: { min: "320px", max: "440px" },
+      ss: { min: "440px", max: "640px" },
+      sm: "640px",
+      md: "768px",
+      lg: "960px",
+      xl: "1024px",
+      xxl: "1200px",
+      img: { min: "1200px", max: "1376px" },
     },
     extend: {
       colors: {
-        primary: '#131424',
-        secondary: '#393A47',
-        accent: '#F13024',
+        primary: "#131424",
+        secondary: "#393A47",
+        accent: "#F13024",
+        fontFamily: {},
+        fontSize: {},
+        fontWeight: {},
+        lineHeight: {},
+        borderRadius: {},
+        colors: {},
+        spacing: {},
+        width: {},
+        minWidth: {},
+        maxWidth: {},
+        height: {},
+        minHeight: {},
+        maxHeight: {},
       },
       backgroundImage: {
         explosion: 'url("/")',
@@ -32,18 +44,18 @@ module.exports = {
         site: 'url("/")',
       },
       animation: {
-        'spin-slow': 'spin 12s linear infinite',
+        "spin-slow": "spin 12s linear infinite",
       },
       fontFamily: {
-        poppins: [`var(--font-poppins)`, 'sans-serif'],
-        sora: [`var(--font-sora)`, 'sans-serif'],
+        poppins: [`var(--font-poppins)`, "sans-serif"],
+        sora: [`var(--font-sora)`, "sans-serif"],
       },
     },
   },
   container: {
     padding: {
-      DEFAULT: '15px',
+      DEFAULT: "15px",
     },
   },
-  plugins: [require('tailwind-scrollbar')],
+  plugins: [require("tailwind-scrollbar")],
 };
